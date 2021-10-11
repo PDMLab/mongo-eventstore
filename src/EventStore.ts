83a1716ee6d9821f0295f3851638faee3de4c169
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-10-11 10:01:35 +0200

    docs: add jsdoc to event store definition (#6)

--- src/EventStore.ts
+++ src/EventStore.ts
@@ -2,14 +2,34 @@ import { Projection } from './MongoDbEventStore'
 import { Event } from './Event'
 
 export type EventStore = {
+  /**
+   * Reads the events of a stream by its `streamId`
+   * @param {string} streamId
+   * @returns Promise<Event[]>
+   */
   readStream: (streamId: string) => Promise<Event[]>
+
+  /**
+   * reads all events of all streams
+   * @param {options} [options]
+   * @param {number} [options.batchSize]
+   */
   readAllEvents: (options?: { batchSize?: number }) => Promise<Event[]>
   appendToStream: (
     streamId: string,
     eventData: Event | Event[],
     options?: { expectedVersion?: number }
   ) => Promise<void>
+  /**
+   * builds all projections
+   * @returns Promise<Projection[]>
+   */
   aggregateAll: () => Promise<Projection[]>
+  /**
+   * builds the projections for an array of events and the projector function
+   * @param {Event[]} events - the events to project
+   * @param {Function} project - the projection function
+   */
   aggregateStream<Projection, SomeEvent extends Event>(
     events: SomeEvent[],
     project: (
